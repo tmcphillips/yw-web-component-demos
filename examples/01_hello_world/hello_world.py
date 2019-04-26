b84932b48358b7c3193d953d41559a290a8897f0
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-04-25 22:35:51 -0700

    added first example script with YW markup

---
+++ examples/01_hello_world/hello_world.py
@@ -0,0 +1,9 @@
+# @begin hello_world @desc Exercise YW and NW with a classic program.
+# @out greeting @desc Greeting displayed to user.
+
+# @begin print_greeting @desc Greet the program user.
+# @out greeting @file stream:stdout
+print("Hello world!")
+# @end print_greeting
+
+# @end hello_world
